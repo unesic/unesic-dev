5ff9c8dcce3e97cfa83da5dc319e52d14790140c
Uros Nesic <unesi98@gmail.com>
2021-10-21 02:44:29 +0200

    theme configuration, rough wireframe laid out

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,43 +1,58 @@
+const path = require("path");
+
 module.exports = {
-  siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "unesic.io",
-  },
-  plugins: [
-    "gatsby-plugin-postcss",
-    "gatsby-plugin-image",
-    {
-      resolve: "gatsby-plugin-google-analytics",
-      options: {
-        trackingId: "",
-      },
-    },
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        icon: "src/images/icon.png",
-      },
-    },
-    "gatsby-plugin-mdx",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "pages",
-        path: "./src/pages/",
-      },
-      __key: "pages",
-    },
-  ],
+	siteMetadata: {
+		siteUrl: "https://unesic.io/",
+		title: "unesic.io",
+	},
+	plugins: [
+		{
+			resolve: "gatsby-plugin-root-import",
+			options: {
+				src: path.join(__dirname, "src"),
+				pages: path.join(__dirname, "src/pages"),
+				components: path.join(__dirname, "src/components"),
+			},
+		},
+		"gatsby-plugin-image",
+		{
+			resolve: "gatsby-plugin-google-analytics",
+			options: {
+				trackingId: "TODO: CHANGE THIS",
+			},
+		},
+		"gatsby-plugin-react-helmet",
+		"gatsby-plugin-sitemap",
+		{
+			resolve: "gatsby-plugin-manifest",
+			options: {
+				icon: "src/images/icon.png",
+			},
+		},
+		"gatsby-plugin-sharp",
+		"gatsby-transformer-sharp",
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				name: "images",
+				path: "./src/images/",
+			},
+			__key: "images",
+		},
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				name: "pages",
+				path: "./src/pages/",
+			},
+			__key: "pages",
+		},
+		{
+			resolve: "@chakra-ui/gatsby-plugin",
+			options: {
+				resetCSS: true,
+				isUsingColorMode: true,
+			},
+		},
+	],
 };
---
+++ src/pages/404.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+const NotFoundPage: React.FC = () => {
+	return <main></main>;
+};
+
+export default NotFoundPage;
